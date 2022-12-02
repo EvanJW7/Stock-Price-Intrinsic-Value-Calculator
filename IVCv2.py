bcc69f3fab182a7960a859c0b6d6f1433d235d5d
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-12-02 11:39:24 -0600

    Update IVCv2.py

--- IVCv2.py
+++ IVCv2.py
@@ -3,13 +3,14 @@ import yfinance as yf
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
 
-print("{:<6} {:>13} {:>17} {:>15} {:>12} {:>17}".format('Stock', 'Est.Growth', 'Intrinsic Value', 'Current Price', 'Discount', 'Recommendation'))
+print("{:<6} {:>13} {:>17} {:>15} {:>12} {:>17}".format('Stock', 'Est.Growth', 'Intrinsic Value', 'Current Price',
+                                                        'Discount', 'Recommendation'))
 print('--------------------------------------------------------------------------------------')
 
 name, price, value, ratio = [], [], [], []
 
 #ENTER DESIRED STOCK TICKERS HERE
-stocks = ['TSLA']
+stocks = ['GS', 'JPM', 'MS', 'C', 'AMD', 'T', 'PTON', 'BTU', 'XOM']
 
 def main():
     for stock in stocks:
@@ -19,9 +20,9 @@ def main():
             headers = {'Accept': 'text/html'}
             res = requests.get(url, headers=headers)
             soup = BeautifulSoup(res.text, 'lxml')
-            PE = soup.find_all('td', class_='text-right')[164]
-            PEG = soup.findAll('td', class_='text-right')[166]
-            LTGrowth = (float(PE.text) / float(PEG.text))
+            pe = soup.find_all('td', class_='text-right')[164]
+            peg = soup.findAll('td', class_='text-right')[166]
+            lt_growth = (float(pe.text) / float(peg.text))
         except:
             try:
                 url = f'https://www.marketwatch.com/investing/stock/{stock}/analystestimates?mod=mw_quote_tab'
@@ -37,56 +38,13 @@ def main():
                 while i < len(eps_estimates):
                     growth.append(((eps_estimates[i]-eps_estimates[i-1])/abs(eps_estimates[i-1]))*100)
                     i += 1
-                LTGrowth = (sum(growth)/len(growth))/2
+                lt_growth = (sum(growth)/len(growth))/2
             except:
                 continue
 
-        # PREVIOUS OPERATING CASH GROWTH
-        try:
-            url = f'http://www.aastocks.com/en/usq/analysis/company-fundamental/cash-flow?symbol={stock}'
-            res = requests.get(url)
-            soup = BeautifulSoup(res.text, 'lxml')
-            points = []
-            x = 130
-            while x < 135:
-                points.append(float(soup.findAll('td')[x].text.replace(',', '')))
-                x += 1
-            growth = []
-            x = 1
-            while x < len(points):
-                growth.append((points[x]-points[x-1])/abs(points[x-1])*100)
-                x += 1
-            operating_cash_growth = round(sum(growth)/len(growth), 2)
+        actual_growth = lt_growth
 
-        except:
-            operating_cash_growth = LTGrowth
-
-        # PREVIOUS EPS GROWTH
-        try:
-            url = f'https://www.reuters.com/companies/{stock}.OQ/key-metrics'
-            res = requests.get(url)
-            soup = BeautifulSoup(res.text, 'lxml')
-            PrevEPSGrowth = float(soup.findAll('span',
-                                               class_="TextLabel__text-label___3oCVw TextLabel__black___2FN-Z TextLabel__regular___2X0ym digits MarketsTable-value-FP5ul")[
-                                      115].text)
-        except:
-            PrevEPSGrowth = operating_cash_growth
-
-        try:
-            x = 0
-            flag = 0
-            while x < len(points)-1:
-                if points[x] > points[x+1]:
-                    flag += 1
-                x += 1
-            if flag > 0:
-                actual_growth = LTGrowth * .75 + operating_cash_growth * .125 + PrevEPSGrowth * .125
-            else:
-                actual_growth = LTGrowth * .34 + operating_cash_growth * .33 + PrevEPSGrowth * .33
-        except:
-            actual_growth = operating_cash_growth
-
-            # DIVIDEND
+        # DIVIDEND
         try:
             url = f'https://www.marketwatch.com/investing/stock/{stock}?mod=quote_search'
             res = requests.get(url)
@@ -221,12 +179,12 @@ def main():
 
         actual_growth = round(actual_growth * 100, 2)
 
-        marketcap = int(round((current_price * shares_outstanding) / 1000000000, 0))
+        marketCap = int(round((current_price * shares_outstanding) / 1000000000, 0))
         intrinsic_market_cap = int(round((intrinsic_value * shares_outstanding) / 1000000000, 2))
 
         print(f"{symbol:<5}{actual_growth:>12}%{intrinsic_value:>16}{current_price:>16}{discount:>15}%{recommendation:>17}")
         name.append(symbol)
-        price.append(marketcap)
+        price.append(marketCap)
         value.append(intrinsic_market_cap)
         ratio.append(intrinsic_value / current_price)
 
